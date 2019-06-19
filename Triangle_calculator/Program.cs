ced124cfc43fb42e8073494311ea8a4049196964
Harshil Kabra <kabraharshil@gmail.com>
2019-06-19 14:48:45 -0400

    Created TriangleSolver class and written basic implementation

--- Triangle_calculator/Program.cs
+++ Triangle_calculator/Program.cs
@@ -10,7 +10,47 @@ namespace Triangle_calculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Triangle calculator");
+            while (true)
+            {
+                int choice;
+                int[] sides = new int[3];
+                String demo;
+                try { 
+                do { 
+                    Console.WriteLine("1. Enter triangle dimensions");
+                    Console.WriteLine("2. Exit");
+                    Console.WriteLine("Enter your choice: ");
+                    choice = int.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:    
+                                for (int i = 0; i < 3; i++)
+                                {
+                                    do
+                                    {
+                                        Console.WriteLine($"Enter length of side-{i + 1}: ");
+                                        sides[i] = int.Parse(Console.ReadLine());
+                                    } while (sides[i] <= 0);
+                                }
+                                Console.WriteLine("Triangle can be formed");
+                                demo = TriangleSolver.Analyze(sides[0], sides[1], sides[2]);
+                                Console.WriteLine(demo);
+                                break;
 
+                        case 2:Environment.Exit(0);
+                            break;
+
+                        default: Console.WriteLine("Please enter a valid choice");
+                            break;
+                    }
+                } while (choice != 1 || choice != 2);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid input\nTry again");
+                }
+            }
         }
     }
 }
---
+++ Triangle_calculator/TriangleSolver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_calculator
+{
+    static class TriangleSolver
+    {
+        
+        private static int side1, side2, side3;
+        private static string demo;
+
+
+        public static int Side1
+        {
+            get { return side1; }
+            set { side1 = value; }
+        }
+
+        public static int Side2
+        {
+            get { return side2; }
+            set { side2 = value; }
+        }
+
+        public static int Side3
+        {
+            get { return side3; }
+            set { side3 = value; }
+        }
+
+        public static string Analyze(int x, int y, int z) {
+            Side1 = x;
+            Side2 = y;
+            Side3 = z;
+            if (x == y && y == z)
+            {
+                demo = "It is an equilateral triangle";
+            }
+            else if (x==y || x==z || y==z)
+            {
+                demo = "It is an isoceles triangle";
+            }
+            else
+            {
+                demo = "It is a scalene triangle";
+            }
+            return demo;
+        }
+    }
+}
